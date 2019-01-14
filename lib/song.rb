2e1b0c9cb8952af3548c76e38cc6da9f9ff0e5e1
Stephanie Abud <sabud1208@gmail.com>
2019-01-14 17:47:46 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song 
+ @@count = 0
+attr_accessor :name, :artist, :genre
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+   @@count +=1
+end
+
+end

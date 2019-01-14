9f6cb7d4fcf04b4868f1f178a69b410cd934dbfb
Stephanie Abud <sabud1208@gmail.com>
2019-01-14 17:49:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,16 @@
 class Song 
  @@count = 0
+ @@artists = 0 
+ @@genres = 0
 attr_accessor :name, :artist, :genre
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
    @@count +=1
+   @@artists += 1 
+   @@genres +=1
+   
 end
 
 end

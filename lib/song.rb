d591eff2b6b6c3d09463797f13eca1af18e4876d
Stephanie Abud <sabud1208@gmail.com>
2019-01-14 18:43:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,13 +26,16 @@ def self.genres
   @@genres.uniq
 end
 
-def self.genre_count
-  genre_count = {}
-  @@genres.each do |x|
-if genre_count[x]
-  genre_count += 1 
-else genre_count = 1
-   end
+ def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
 end
 
